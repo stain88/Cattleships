a2e7adfb4946af9b1ece5b859c5d76d6c7fd3324
GowsK <gowsikiri@hotmail.co.uk>
2015-12-07 21:58:15 +0000

    routing added and css

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 angular
     .module('cattleshipsApp', ['firebase', 'ui.router'])
     .config(MainRouter);
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+       url: "/home",
+       templateUrl: "home.html"
+  })
+    .state('archive', {
+      url: "/archive",
+      templateUrl: "archive.html"
+  })
+    .state('landing', {
+      url: "/",
+      templateUrl: "landing.html"
+  })
+    .state('instructions', {
+      url: "/instructions",
+      templateUrl: "instructions.html"
+  });
+
+  $urlRouterProvider.otherwise("/");
+       
+
+}
