6317cd04af96fc96dfbd29b41537722e7c752aec
stain88 <marcbaghdadi@gmail.com>
2015-12-07 11:35:56 +0000

    squares up

--- js/cattleshipsController.js
+++ js/cattleshipsController.js
@@ -10,12 +10,16 @@ function cattleshipsController() {
     size = size || 10;
     board = {};
     for (var i=0;i<size;i++) {
-      board["row"+i] = {"columns":{}};
+      board["row"+i] = {id: i, "columns":{}};
       for (var j=0;j<size;j++) {
         board["row"+i]["columns"]["column"+j]="";
       };
     };
     console.log(board);
     self.board = board;
+  }
+
+  self.can_play = function(row_id, index) {
+    console.log("row: "+row_id, "index: "+index);
   }
 }
